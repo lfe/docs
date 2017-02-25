9af60de115c0c3d2d67ac7434e908e2470b23099
Duncan McGreggor <oubiwann@gmail.com>
2017-02-25 02:32:26 -0600

    Refeactored (and fixed) make targets.

--- priv/make/gen.mk
+++ priv/make/gen.mk
@@ -4,6 +4,7 @@ DOCS_STABLE_BUILD_DIR = $(DOCS_ROOT)/current
 DOCS_DEV_BUILD_DIR = $(DOCS_ROOT)/dev
 SASS = sass --no-cache -f --trace
 SASS_MIN = $(SASS) --style compressed
+SASS_WATCH = sass --no-cache --watch --trace
 
 sass:
 	sudo gem update --system
@@ -27,33 +28,41 @@ css-stable: css
 css-1.3: DEPLOYMENT = v1.3
 css-1.3: css
 
-docs-dev: clean compile css-dev
+css-watch:
+	@$(SASS_WATCH) $(SASS_DIR)/lfe-$(DEPLOYMENT).scss:$(DOCS_ROOT)/$(DEPLOYMENT)/css/bootstrap-min.css &
+
+css-watch-dev: DEPLOYMENT = dev
+css-watch-dev: css-watch
+
+docs-header:
 	@echo "\nBuilding docs ..."
 	@echo
-	@ERL_LIBS=$(ERL_LIBS) erl -s docs -s docs-gen run-dev -noshell -eval 'init:stop()'
 
-docs-stable: clean compile css-stable
-	@echo "\nBuilding docs ..."
-	@echo
-	@ERL_LIBS=$(ERL_LIBS) erl -s docs -s docs-gen run -noshell -eval 'init:stop()'
+docs-dev-only: docs-header
+	@ERL_LIBS=$(ERL_LIBS) erl -s docs-cli gen-dev -noshell -eval 'init:stop()'
 
-docs-only:
-	@echo "\nBuilding docs ..."
-	@echo
-	@ERL_LIBS=$(ERL_LIBS) erl -s docs -s docs gen-content -noshell -eval 'init:stop()'
+docs-stable-only: docs-header
+	@ERL_LIBS=$(ERL_LIBS) erl -s docs-cli gen-stable -noshell -eval 'init:stop()'
 
-devdocs: docs
+docs-dev: docs-header clean compile css-dev docs-dev-only
+
+docs-stable: docs-header clean compile css-stable docs-stable-only
+
+serve-header:
 	@echo "\nRunning docs server on http://$(LOCAL_DOCS_HOST):$(LOCAL_DOCS_PORT) ... (To quit, hit ^c twice)"
 	@echo
+
+serve-only:
+	@ERL_LIBS=$(ERL_LIBS) erl -s docs-cli httpd -noshell
+
+serve: compile serve-header serve-only
+
+serve-watch-css: serve-header css-watch-dev serve-only
 	@ERL_LIBS=$(ERL_LIBS) erl -s docs -s docs gen-content -s docs httpd -noshell
+
+serve-dev: docs-dev serve-watch-css
 
 contributors:
 	git log --pretty=format:"%an"|sort -u
-
-devcss:
-	@echo "\nRunning docs server on http://$(LOCAL_DOCS_HOST):$(LOCAL_DOCS_PORT) ... (To quit, hit ^c twice)"
-	@echo
-	@sass --watch $(SASS_DIR)/lfe-theme.scss:$(CSS_BUILD_DIR)/bootstrap-min.css &
-	@ERL_LIBS=$(ERL_LIBS) erl -s docs -s docs gen-content -s docs httpd -noshell
 
 .PHONY: docs
